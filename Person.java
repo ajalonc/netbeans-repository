825e1ea64176c38ad8e5148b229000d1255f6fc0
TRobinson57 <TRobinson57@users.noreply.github.com>
2018-01-29 16:04:27 -0600

    added Person File, still needs to updated

---
+++ Person.java
@@ -0,0 +1,96 @@
+/*
+ *Author: Timothy Robinson
+ *Date:
+ *Assignment: Assignment .java
+ *Description:
+ */
+
+ 
+public class Person {
+
+        //data members
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+
+
+
+            //default constructor
+    public Person() {
+    }
+
+
+            //override constructor
+    public Person(String firstName, String lastName, String address, String city, String state, String zipCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " " + address + " " +
+                city + " " + state + " " + zipCode;
+    }
+
+
+
+
+
+}       //end class
