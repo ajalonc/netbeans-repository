4927891a9fa8e9244ba3ed49d572617c30e3567f
ajalonc <ajaloncorcoran@yahoo.com>
2018-01-29 17:27:27 -0600

    Add files via upload

---
+++ Assinment2GroupProject/src/MyData.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ajalon
+ */
+public class MyData {
+    
+    private int month;
+    private int day;
+    private int year;
+
+    public MyData() {
+    }
+
+    public MyData(int month, int day, int year) {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return  month + "/" + day + "/" + year;
+    }
+    
+    
+    
+    
+    
+}
